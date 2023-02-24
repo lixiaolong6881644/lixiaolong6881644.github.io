c187d4703b7d14ac0c81c7410de84497c9fdb970
lixiaolong2 <lixiaolong2@rock-ai.com>
2023-02-24 14:34:17 +0800

    Site updated: 2023-02-24 14:34:15

--- js/myDiyJs.js
+++ js/myDiyJs.js
@@ -1,10 +1,8 @@
 var xiaokang = new xkTool();
 // 设置随机背景的图片
 xiaokang.imgList = [
-    "https://ae01.alicdn.com/kf/Uc32240fb1b134adc8b256577bd78b9a3j.jpg",
     "https://ae01.alicdn.com/kf/H21b5f6b8496141a1979a33666e1074d9x.jpg",
     "https://lxiaol.oss-cn-beijing.aliyuncs.com/pictures/fluid_banner.png",
-    "https://pic.downk.cc/item/5fdab6363ffa7d37b30d2d5a.png",
     "https://pic.downk.cc/item/5fdab6363ffa7d37b30d2d5d.jpg",
     "https://img.imgdb.cn/item/6088f2f3d1a9ae528f384101.png",
     "https://pic.imgdb.cn/item/609e0186d1a9ae528ffff80c.jpg",
@@ -13,15 +11,14 @@ xiaokang.imgList = [
     "https://pic.imgdb.cn/item/609dfeafd1a9ae528fe3a40a.jpg",
     "https://pic.imgdb.cn/item/600becac3ffa7d37b3b55fa6.png",
     "https://lxiaol.oss-cn-beijing.aliyuncs.com/pictures/%E5%B0%8F%E7%8B%90%E7%8B%B8%E7%9B%AF.png", // 小狐狸-盯
-    "https://lxiaol.oss-cn-beijing.aliyuncs.com/pictures/%E5%A8%9C%E5%8F%AF%E9%9C%B2%E9%9C%B2-%E5%89%8D%E5%B0%98%E9%95%9C1920x1080.png", // 娜可露露-前尘镜
+    // "https://lxiaol.oss-cn-beijing.aliyuncs.com/pictures/%E5%A8%9C%E5%8F%AF%E9%9C%B2%E9%9C%B2-%E5%89%8D%E5%B0%98%E9%95%9C1920x1080.png", // 娜可露露-前尘镜
     "https://lxiaol.oss-cn-beijing.aliyuncs.com/pictures/%E5%90%AC%E9%A3%8E%E8%80%85_2021-04-08_21-45-17.jpg", // 听风者
-    "https://lxiaol.oss-cn-beijing.aliyuncs.com/pictures/%E4%B9%9E%E5%B7%A7%E7%BB%87%E6%83%85.jpg", // 乞巧织情
+    // "https://lxiaol.oss-cn-beijing.aliyuncs.com/pictures/%E4%B9%9E%E5%B7%A7%E7%BB%87%E6%83%85.jpg", // 乞巧织情
     "https://lxiaol.oss-cn-beijing.aliyuncs.com/pictures/%E8%B1%B9%E5%AD%90%E5%A4%B4%E5%85%A8%E9%83%BD%E8%A6%81.gif", // 豹子头全都要
     "https://lxiaol.oss-cn-beijing.aliyuncs.com/pictures/%E6%B1%9F%E6%B9%96%E5%84%BF%E5%A5%B3_2021-04-22_23-12-04.jpg", // 江湖儿女
     "https://lxiaol.oss-cn-beijing.aliyuncs.com/pictures/%E5%85%AC%E5%9B%AD.png", // 公园
     "https://lxiaol.oss-cn-beijing.aliyuncs.com/pictures/bg14.jpg",
-    "https://pic.imgdb.cn/item/6108ac4d5132923bf8ca61c3.png", //小狐狸
-    "https://pic.imgdb.cn/item/6126fbbd44eaada739d33cad.jpg", //大桥白鹤梁
+    // "https://pic.imgdb.cn/item/6126fbbd44eaada739d33cad.jpg", //大桥白鹤梁
     "https://pic.downk.cc/item/5fdab63e3ffa7d37b30d320e.png", //宇航员
     "https://p6.toutiaoimg.com/img/tos-cn-i-siecs4i2o7/49741d64ee9f4d24bf4c1c2406832fa7~noop.image", //游戏机
     "https://p6.toutiaoimg.com/img/tos-cn-i-siecs4i2o7/c4bdd673c30147a290a8dc1f25ad1e62~noop.image", //INTER NET
